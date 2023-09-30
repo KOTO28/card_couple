fe774791f1e468c8a2ff119f4d3234f99b564337
KOTO28 <k.tonton128@gmail.com>
2023-09-30 09:26:26 +0900

    update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const card_sx = 80;
 const card_sy = 128;
+const card_px = 4;
 
 window.onload = function(){
 	console.log("Hello World!");
@@ -33,15 +34,15 @@ window.onload = function(){
 			if(select != -1){
 				ctx.beginPath();
 				ctx.rect((select%4)*card_sx+40, Math.floor(select/4)*card_sy,card_sx,card_sy);
-				ctx.strokeStyle = 'deepskyblue';
+				ctx.strokeStyle = '#00f';
 				ctx.lineWidth = 4;
 				ctx.stroke();
 			}
 		},
 	};
 	cv.addEventListener("click",function(event){
-		console.log("clicked!!");
-		console.log("x:"+event.pageX+",y:"+event.pageY);
+		// console.log("clicked!!");
+		// console.log("x:"+event.pageX+",y:"+event.pageY);
 		let nai = true;
 		// c ++;
 		// card.add(((c-1)%4)*card_sx,Math.floor((c-1)/4)*card_sy,getRandom(1,13));
@@ -52,14 +53,66 @@ window.onload = function(){
 				Math.floor(i/4)*card_sy < event.pageY &&
 				event.pageY < Math.floor(i/4)*card_sy+card_sy
 			){
-				console.log("click num:"+i);
-				console.log("click is:"+card.data[i]);
-				if(select != -1){
+				// console.log("click num:"+i);
+				// console.log("click is:"+card.data[i]);
+				if(select != -1 && select != i){
+					// console.log("a");
 					if(card.data[select] == card.data[i]){
-						card.del(select);
-						card.del(i);
-						select = -1;
-						return;
+						// console.log("b");
+						let jouken = false;
+						if(//左端
+							select % card_px == 0
+						){
+							console.log("左端");
+							if(
+								i == select-card_px ||//上
+								i == select-card_px+1 ||//右上
+								i == select+1 ||//右
+								i == select+card_px ||//下
+								i == select+card_px+1//右下
+							){
+								jouken = true
+							}
+						}else if(//右端
+							select % card_px == 3
+						){
+							console.log("右端");
+							if(
+								i == select-card_px ||//上
+								i == select-card_px-1 ||//左上
+								i == select-1 ||//左
+								i == select+card_px ||//下
+								i == select+card_px-1//左下
+							){
+								jouken = true
+							}
+						}else{
+							console.log("真ん中");
+							if(
+								i == select-card_px-1 ||//左上
+								i == select-card_px ||//上
+								i == select-card_px+1 ||//右上
+								i == select+1 ||//右
+								i == select-1 ||//左
+								i == select+card_px-1 ||//左下
+								i == select+card_px ||//下
+								i == select+card_px+1//右下
+							){
+								jouken = true
+							}
+						}
+						if(jouken){
+							// console.log("c");
+							if(select < i){
+								card.del(i);
+								card.del(select);
+							}else{
+								card.del(select);
+								card.del(i);
+							}
+							select = -1;
+							return;
+						}
 					}
 				}
 				select = i;
@@ -77,9 +130,9 @@ window.onload = function(){
 			event.preventDefault();//スクロールを無効化
 		}
 	});
-	btn.addEventListener("click",function(event){
-		card.del(select);
-	});
+	// btn.addEventListener("click",function(event){
+	// 	card.del(select);
+	// });
 	loop();
 	// let c = 0;
 	function update(){
